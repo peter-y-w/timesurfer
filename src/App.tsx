fbfcaa85b8359ed4484a9f5e52d8ea4719ba59cf
Peter Wang <peter.wang@findex.com.au>
2025-08-11 11:19:53 +1000

    feat: place TimerCard in grid container

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
-import styles from './App.module.css';
-import TimerCard from './components/TimerCard';
+import styles from "./App.module.css";
+import TimerCard from "./components/TimerCard";
+import { Grid } from "@mui/material";
 
 function App() {
-	return (
-		<div className={styles.container}>
-			<TimerCard />
-		</div>
-	);
+  return (
+    <div className={styles.appContainer}>
+      <Grid
+        container
+        sx={{
+          justifyContent: "center",
+          alignItems: "center",
+          paddingTop: "5rem",
+        }}
+      >
+        <TimerCard />
+      </Grid>
+    </div>
+  );
 }
 
 export default App;
