3d5ea8090d562e4f2c14cf788099b4639632f042
Peter Wang <peter.wang@findex.com.au>
2025-08-11 12:52:31 +1000

    feat: working stopwatch display and start stop functionality

--- src/components/TimerCard.tsx
+++ src/components/TimerCard.tsx
@@ -1,11 +1,52 @@
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import styles from "./TimerCard.module.css";
+import { Box, Button, Grid, Stack, Typography } from "@mui/material";
+import { useEffect, useRef, useState } from "react";
 
 const TimerCard = () => {
+    const [focusTime, setFocusTime] = useState(0);
+    const [isFocusing, setIsFocusing] = useState(false);
+    const intervalRef = useRef<number>(null);
+
+    useEffect(() => {
+        if (isFocusing) {
+            intervalRef.current = setInterval(() => {
+                setFocusTime((prev) => prev + 1);
+            }, 1000);
+        } else {
+            stopTimer();
+        }
+
+        return () => {
+            stopTimer();
+        };
+    }, [isFocusing]);
+
+    const stopTimer = () => {
+        if (intervalRef.current) clearInterval(intervalRef.current);
+    };
+
+    const toggleFocusing = () => setIsFocusing((prev) => !prev);
+
     return (
-        <Card className={styles.timerCard} elevation={0}>
-            <CardContent></CardContent>
+        <Card elevation={0}>
+            <CardContent className={styles.timerCard}>
+                <Grid
+                    container
+                    direction="column"
+                    sx={{
+                        justifyContent: "center",
+                        alignItems: "center",
+                    }}
+                >
+                    <Typography className={styles.timerDisplay} variant="h1" sx={{ fontFamily: "math" }}>
+                        {focusTime}
+                    </Typography>
+
+                    <Button onClick={toggleFocusing}>{isFocusing ? "Stop" : "Start"}</Button>
+                </Grid>
+            </CardContent>
         </Card>
     );
 };
